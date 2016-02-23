0c61cfdc4d4dc777d3b75c1f5ba3e2c18676018f
francois <administrador@CU-FCHATAIGNER>
2016-02-23 14:33:25 +0100

    disable repeats by default

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,16 @@ int main(int argc, char** argv)
   ros::Publisher pub_down = n.advertise<keyboard::Key>("keydown", 10);
   ros::Publisher pub_up = n.advertise<keyboard::Key>("keyup", 10);
 
+  bool allow_repeat=false;
   int repeat_delay, repeat_interval;
   
+  n.param<bool>( "allow_repeat", allow_repeat, false ); // disable by default
   n.param<int>( "repeat_delay", repeat_delay, SDL_DEFAULT_REPEAT_DELAY );
   n.param<int>( "repeat_interval", repeat_interval, SDL_DEFAULT_REPEAT_INTERVAL );
   
+  if ( !allow_repeat ) repeat_delay=0; // disable 
   keyboard::Keyboard kbd( repeat_delay, repeat_interval );
+  
   ros::Rate r(50);
   
   keyboard::Key k;
