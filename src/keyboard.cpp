5c4f22e9e022bcfa7b7e79a9cb58e1d06fa72fa4
Igor Zubrycki <igor@gag-film.com>
2017-07-08 13:34:44 +0200

    add color when keydown/keyup

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,11 +1,14 @@
 #include "keyboard.h"
-
+#include <iostream>
+#include <cmath>
 keyboard::Keyboard::Keyboard( int repeat_delay, int repeat_interval )
 {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) throw std::runtime_error("Could not init SDL");
   SDL_EnableKeyRepeat( repeat_delay, repeat_interval );
   SDL_WM_SetCaption("ROS keyboard input", NULL);
   window = SDL_SetVideoMode(100, 100, 0, 0);
+  
+
 }
 
 keyboard::Keyboard::~Keyboard(void)
@@ -26,12 +29,17 @@ bool keyboard::Keyboard::get_key(bool& new_event, bool& pressed, uint16_t& code,
         code = event.key.keysym.sym;
         modifiers = event.key.keysym.mod;
         new_event = true;
+        SDL_FillRect(window, NULL,SDL_MapRGB(window->format, code, 0, 0));  
+        SDL_Flip(window);   
       break;
       case SDL_KEYDOWN:
         pressed = true;
         code = event.key.keysym.sym;
         modifiers = event.key.keysym.mod;
         new_event = true;
+        SDL_FillRect(window, NULL, SDL_MapRGB(window->format,code,0,255)); 
+        SDL_Flip(window);
+
       break;
       case SDL_QUIT:
         return false;
